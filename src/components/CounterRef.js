5a3a46015912f99c27ce2aa3a6459e0900d0ec96
munenebrian <njerubrianmunene@gmail.com>
2022-07-20 20:43:58 +0300

    coded along Ticker.js!

--- src/components/CounterRef.js
+++ src/components/CounterRef.js
@@ -1,19 +1,18 @@
-import React, { useRef } from "react";
+import React, { useState } from "react";
 
-function CounterRef() {
-  const count = useRef(0);
+function CounterState() {
+  const [count, setCount] = useState(0);
 
   function handleClick() {
-    count.current = count.current + 1;
-    console.log(count.current);
+    setCount((count) => count + 1);
   }
 
   return (
     <div>
-      <h1>CounterRef</h1>
-      <button onClick={handleClick}>{count.current}</button>
+      <h1>CounterState</h1>
+      <button onClick={handleClick}>{count}</button>
     </div>
   );
 }
 
-export default CounterRef;
+export default CounterState;
